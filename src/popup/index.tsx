8950f2567dc0c4fa5e5223d2220bbbae011581e3
Keishi Asai <isheik808@gmail.com>
2023-02-23 02:23:23 -0800

    add packages

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -4,3 +4,9 @@ import { createRoot } from "react-dom/client";
 import Popup from "./Popup";
 
 import "../tailwind.css";
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Popup />
+  </React.StrictMode>
+);
